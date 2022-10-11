2cbcbac5eada2664349601598dfc9794624276ac
vikri <vikriagung7@gmail.com>
2022-10-11 16:47:55 +0700

    Add update data in cart and fixing currency format in checkout and prescription page

--- projects/client/src/pages/TransactionListPage.jsx
+++ projects/client/src/pages/TransactionListPage.jsx
@@ -13,7 +13,9 @@ import {
 } from "@chakra-ui/react";
 import SearchBar from "../components/SearchBar";
 import { HiChevronDown } from "react-icons/hi";
-import Navbar from "../components/NavbarComponent";
+import { FiShoppingBag } from "react-icons/fi";
+
+
 
 export default function TransactionListPage() {
 
@@ -40,14 +42,14 @@ export default function TransactionListPage() {
 
                         <div className="py-3">
                             <div className="pb-3">
-                              <label className="py-3">From :</label>
-                            <Input
-                                type="date"
-                                min="1922-01-01"
-                                max="2022-09-29"
-                            />  
+                                <label className="py-3">From :</label>
+                                <Input
+                                    type="date"
+                                    min="1922-01-01"
+                                    max="2022-09-29"
+                                />
                             </div>
-                            
+
                             <label >To :</label>
                             <Input
                                 type="date"
@@ -83,10 +85,55 @@ export default function TransactionListPage() {
                             </Menu>
                         </div>
                         {/* Right Area : Product Carousel with sort */}
-                        <div className="border w-3/4 mx-auto">
-                            Transaction list
-                        </div>
+                        <div className="border rounded w-3/4 mx-auto p-6 bg-white ">
+                            <div className="flex justify-between border-b pb-2">
+                                <div className=" flex">
+                                    <div className="flex items-center mx-2">
+                                        <FiShoppingBag />
+                                    </div>
 
+                                    <div className="flex items-center mx-2">
+                                        <p>22 September 2022</p>
+                                    </div>
+
+                                    <div className="flex items-center mx-2">
+                                        <p>INV/2022021290</p>
+                                    </div>
+
+                                </div>
+
+                                <div className="rounded font-bold text-[#37ba69] flex items-center bg-[#d6ffde] px-2 mx-2">
+                                    Processed
+                                </div>
+                            </div>
+
+                            <div className="flex justify-between">
+                                <div className="flex">
+                                    <div className="flex items-center">
+                                        <img className="w-[120px]" src="https://d2qjkwm11akmwu.cloudfront.net/products/2a19fc0a-32dc-406c-9860-b4ff3348b10b_product_image_url.webp" alt="img_prod" />
+                                    </div>
+
+                                    <div className="flex items-center row">
+                                        <div>
+                                            <p>Telsat 40mg x 1</p>
+                                            <p className="text-muted text-[12px]">and 1 more product</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center">
+                                    <div className="pr-6 pl-3">
+                                        <p>Total purchase</p>
+                                        <p className="font-bold">Rp200.000,-</p>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div className="flex justify-end pr-6">
+                                <button className="border py-2 px-6 hover:bg-white hover:text-hijauBtn bg-hijauBtn text-white font-bold">See details</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
